4b9a0adc0f16ab0760717dbdd3e7322ac2d612fc
samuelebumbello <samuelebumbello17@gmail.com>
2023-04-03 00:15:51 +0200

    [add] timer number and check

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Elements //
+// Elements \\
 const numbers = [];
 
-// Genero i numeri random (5) //
+// Genero i numeri random (5) \\
 while (numbers.length < 5) {
     const numGen = Math.floor(Math.random() * 100) + 1;
 
@@ -11,8 +11,36 @@ while (numbers.length < 5) {
     console.log(numGen);
 }
 
-// Pusho i numeri random sull'HTML //
+// Pusho i numeri random sull'HTML \\
 const numDiv = document.getElementById('numbers');
-// Restituisco una stringa ai numeri dell'array //
+// Restituisco una stringa ai numeri dell'array \\
 numDiv.innerHTML = numbers.join(' ');
 
+// Creo il TIMER 
+setTimeout(function() {
+    numDiv.innerHTML = '';
+}, 4000);
+
+setTimeout(() => {
+// Chiedo all'utente di inserire i 5 numeri \\
+    const numUser = [];
+    for (let i = 1; i <= 5; i++) {
+    const choiceNum = parseInt(prompt(`Inserisci il numero ${i}`))
+    numUser.push(numUser);
+    console.log(choiceNum);
+    }
+// Verifico quali numeri sono stati indonvinati \\
+    const sameNumber = [];
+    for (const choiceNum of numUser) {
+        if (numbers.includes(numUser)) {
+            sameNumber.push(numUser);
+        }
+    }
+
+// Stampo il risultato \\
+    if (sameNumber.length === 0) {
+        document.getElementById('msg2').textContent = (`Riprova, i numeri inseriti non corrispondono.`);
+    }else {
+        document.getElementById('msg1').textContent = (`Sei riuscito a trovare ${sameNumber.length} numeri: ${sameNumber.join(' ')}`);
+    }
+},5000);
